06fc96ea25aed08b07d9dc22a294b6bc7eb19d8b
Anik Islam <aanikbsmrstucse@gmail.com>
2023-09-03 13:59:48 +0600

    login functionality done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
 import Home from '../pages/Home/Home'
+import Login from '../pages/Login/Login'
 
 export const router = createBrowserRouter([
   {
@@ -9,6 +10,13 @@ export const router = createBrowserRouter([
     children: [{
       path: '/',
       element: <Home></Home>
-    }]
+    }],
   },
+  {
+    path:'/login',
+    element:<Login></Login>
+  },
+  {
+    path:'signup',
+  }
 ])
