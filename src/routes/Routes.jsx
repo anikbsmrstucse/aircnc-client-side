5eeffcb5101a97f06dcf7e4648ea204626eedf7b
Anik Islam <aanikbsmrstucse@gmail.com>
2023-09-04 12:36:15 +0600

    registration features all done

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import Main from '../layouts/Main'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Login/Login'
+import Signup from '../pages/Signup/Signup'
 
 export const router = createBrowserRouter([
   {
@@ -18,5 +19,6 @@ export const router = createBrowserRouter([
   },
   {
     path:'signup',
+    element: <Signup></Signup>
   }
 ])
